9f16fa29df3b8d2ef446d3859d94149d700f916b
Code Monk <abhishek2142001@gmail.com>
2022-06-01 14:00:31 +0530

    Update custom_dana_video.js

--- custom_dana_video.js
+++ custom_dana_video.js
@@ -1,6 +1,8 @@
    window.addEventListener( "message",
           function (e) {
+               alert(e.data.messageType)
                 if(e.data.messageType=="videoData"){
+                   
                   window.YellowMessengerPlugin.sendEvent({
                        message:e.data.videoContent 
                      },
